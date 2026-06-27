72c65bf78495f73df782341d445d78d6038e836e
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-06-26 22:00:02 -0300

    criacao do flyway para o versionamento do banco

---
+++ src/main/java/dev/matheusrrt/fightersys/FightersysApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusrrt.fightersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightersysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightersysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheusrrt/fightersys/FightersysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheusrrt.fightersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
